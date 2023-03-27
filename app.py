f5a385fecc8f0439e1a653ba7f2c3b1d7ba95005
modgoviya <112287614+modgoviya@users.noreply.github.com>
2023-03-27 21:43:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,82 @@
-import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-import json
-
-# Set the app title and favicon
-st.set_page_config(page_title='Shopify URL Filter', page_icon=':money_with_wings:')
+import streamlit as st
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm, trange
 
 # Define a function to filter the URLs to only include Shopify sites
-def filter_urls(urls):
-    shopify_urls = []
-    for url in urls:
-        try:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            # Check for Shopify CDN domain
-            if 'cdn.shopify.com' in url or 'checkout.shopify.com' in url:
-                shopify_urls.append(url)
-            # Check for Shopify checkout pages
-            elif '/checkouts/' in url:
-                shopify_urls.append(url)
-            # Check for Shopify favicon
-            elif soup.find('link', {'rel': 'shortcut icon', 'href': '/favicon.ico'}) is not None:
-                shopify_urls.append(url)
-            # Check for Shopify logo
-            elif soup.find('img', {'alt': 'Shopify'}) is not None:
-                shopify_urls.append(url)
-            # Check for Shopify scripts
-            elif soup.find('script', {'src': '/shopify_common.js'}) is not None:
-                shopify_urls.append(url)
-            elif soup.find('script', {'src': '/shopify_pay.js'}) is not None:
-                shopify_urls.append(url)
-            # Check for Shopify apps
-            elif 'shopify_app' in url or 'shopify_plus' in url:
-                shopify_urls.append(url)
-            # Check for Shopify meta tags
-            elif soup.find('meta', {'name': 'generator', 'content': 'Shopify'}) is not None:
-                shopify_urls.append(url)
-        except:
-            pass
-    return shopify_urls
+def filter_urls(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Check for Shopify CDN domain
+        if 'cdn.shopify.com' in url or 'checkout.shopify.com' in url:
+            return url
+        # Check for Shopify checkout pages
+        elif '/checkouts/' in url:
+            return url
+        # Check for Shopify favicon
+        elif soup.find('link', {'rel': 'shortcut icon', 'href': '/favicon.ico'}) is not None:
+            return url
+        # Check for Shopify logo
+        elif soup.find('img', {'alt': 'Shopify'}) is not None:
+            return url
+        # Check for Shopify scripts
+        elif soup.find('script', {'src': '/shopify_common.js'}) is not None:
+            return url
+        elif soup.find('script', {'src': '/shopify_pay.js'}) is not None:
+            return url
+        # Check for Shopify apps
+        elif 'shopify_app' in url or 'shopify_plus' in url:
+            return url
+        # Check for Shopify meta tags
+        elif soup.find('meta', {'name': 'generator', 'content': 'Shopify'}) is not None:
+            return url
+    except:
+        pass
 
-# Set the app title and description
-st.title('Shopify URL Filter')
-st.write('Filter a list of URLs to only include Shopify sites')
+# Define the Streamlit app
+def app():
+    # Set the page title
+    st.set_page_config(page_title='Shopify URL Filter', page_icon=':money_with_wings:')
 
-# Allow users to upload an input file
-st.sidebar.title('Upload Input File')
-input_file = st.sidebar.file_uploader('Choose a file', type=['txt', 'csv'])
+    # Display the app header
+    st.title('Shopify URL Filter')
+    st.write('This app filters a list of URLs to only include Shopify sites.')
 
-# Allow users to download the output file
-st.sidebar.title('Download Output File')
-output_format = st.sidebar.selectbox('Select output file format', ['txt', 'json'])
-if output_format == 'txt':
-    output_file = st.sidebar.empty()
-else:
-    output_file = st.sidebar.download_button('Download JSON', None, file_name='shopify_urls.json')
+    # Create a file uploader for the input file
+    st.sidebar.title('Upload Input File')
+    input_file = st.sidebar.file_uploader('Choose a text file with one URL per line', type=['txt'])
 
-# Filter the URLs to only include Shopify sites
-if input_file:
-    with input_file:
+    # Filter the URLs to only include Shopify sites using multiple threads
+    if input_file is not None:
         urls = input_file.read().splitlines()
+        with ThreadPoolExecutor(max_workers=100) as executor:
+            futures = [executor.submit(filter_urls, url) for url in urls]
 
-    shopify_urls = filter_urls(urls)
+            # Initialize the progress bar
+            pbar = tqdm(total=len(urls), desc='Filtering URLs', unit='urls')
 
-    # Display the number of Shopify URLs found
-    st.write('### Results')
-    st.write(f'Found {len(shopify_urls)} Shopify URLs')
+            # Iterate over the completed futures
+            shopify_urls = []
+            for future in as_completed(futures):
+                url = future.result()
+                if url:
+                    shopify_urls.append(url)
+                pbar.update(1)
+                pbar.set_postfix({'Shopify URLs': len(shopify_urls)})
+            pbar.close()
 
-    # Display the Shopify URLs
-    if len(shopify_urls) > 0:
-        st.write('#### Shopify URLs')
+        # Display the filtered URLs in the app
+        st.write(f'{len(shopify_urls)} Shopify URLs found:')
         for url in shopify_urls:
             st.write(url)
 
-# Save the Shopify URLs to an output file
-if output_format == 'txt':
-    output_file.text('\n'.join(shopify_urls))
-    st.sidebar.success('Output file saved!')
-else:
-    shopify_dict = {'shopify_urls': shopify_urls}
-    output_file.data = json.dumps(shopify_dict, ensure_ascii=False, indent=4).encode('utf-8')
-    st.sidebar.success('Output file saved!')
+        # Allow the user to download the filtered URLs as a text file
+        output_file = st.file_uploader("Download Shopify URLs as text file")
+        if output_file is not None:
+            output_file.write('\n'.join(shopify_urls))
+
+# Run the app
+if __name__ == '__main__':
+    app()
